8025fb1b7c1a1c5c2786bccaf5b9bed41e94d835
Jaro111 <jarokark@protonmail.com>
2024-09-08 17:37:28 +0100

    chore: connection to db is working

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const port = process.env.PORT || 5001;
+const connection = require("../src/db/connection");
 
 const app = express();
 
 app.use(express.json());
+
+connection();
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
