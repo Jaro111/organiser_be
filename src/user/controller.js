c680a869e0ee4e582545113799e829e0ee22f6c9
Jaro111 <jarokark@protonmail.com>
2024-11-04 13:01:38 +0000

    chore: reset request and reset password routes and controllers done

--- src/user/controller.js
+++ src/user/controller.js
@@ -1,6 +1,8 @@
 const User = require("../user/model");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+const sendEmail = require("../utils/sendEmail");
 
 // Register
 const addUser = async (req, res) => {
@@ -101,9 +103,64 @@ const updateUser = async (req, res) => {
   }
 };
 
+const sendResetLink = async (req, res) => {
+  const { email } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Generate reset token
+    const token = crypto.randomBytes(32).toString("hex");
+    const tokenExpiration = Date.now() + 3600000; // 1 hour expiration
+
+    // Save the token and expiration in the user's document
+    user.resetPasswordToken = token;
+    user.resetPasswordExpires = tokenExpiration;
+    await user.save();
+
+    // Send email with the token link
+    const resetUrl = `http://localhost:3000/resetPassordRequest/${token}`;
+    await sendEmail({
+      to: user.email,
+      subject: "Password Reset",
+      text: `You are receiving this because you (or someone else) have requested the reset of the password for your account. Please click the following link to reset your password: ${resetUrl}`,
+    });
+
+    res
+      .status(200)
+      .json({ message: "Password reset link sent to email.", user: user });
+  } catch (error) {
+    res.status(500).json({ error: error, message: error.message });
+  }
+};
+
+const resetPassword = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      resetPasswordToken: req.params.token,
+      resetPasswordExpires: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid or expired token" });
+    }
+    const saltRounds = parseInt(process.env.SALT_ROUNDS);
+    user.password = await bcrypt.hash(req.body.password, saltRounds);
+    await user.save();
+
+    res.status(200).json({ message: "Password has been changed" });
+  } catch (error) {
+    res.status(500).json({ error: error, message: error.message });
+  }
+};
+
 module.exports = {
   addUser: addUser,
   getAllUsers: getAllUsers,
   logIn: logIn,
   updateUser: updateUser,
+  sendResetLink: sendResetLink,
+  resetPassword: resetPassword,
 };
--- src/user/routes.js
+++ src/user/routes.js
@@ -1,7 +1,14 @@
 const { Router } = require("express");
 const userRouter = Router();
 
-const { addUser, getAllUsers, logIn, updateUser } = require("./controller");
+const {
+  addUser,
+  getAllUsers,
+  logIn,
+  updateUser,
+  sendResetLink,
+  resetPassword,
+} = require("./controller");
 const { hashPass, comparePass, tokenCheck } = require("../middleware/auth");
 
 //register new user
@@ -22,5 +29,11 @@ userRouter.get("/user/authCheck", tokenCheck, logIn);
 
 // update User
 userRouter.put("/user/updateUser", tokenCheck, updateUser);
+
+// resetPassword request
+userRouter.post("/user/resetLink", sendResetLink);
+
+// reset password
+userRouter.post("/user/resetPassword/:token", resetPassword);
 
 module.exports = userRouter;
