6fe33298cb3111548a016812408edec110fd2153
Jaro111 <jarokark@protonmail.com>
2025-01-15 16:38:39 +0000

    test

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connection = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI.toString());
     console.log("DB connection is working");
   } catch (error) {
     console.log(error);
