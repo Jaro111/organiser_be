78c9253815679f19b798b82a29dd9fc16c3e7eef
Jaro111 <jarokark@protonmail.com>
2025-01-15 16:43:07 +0000

    test2 mongo uri

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connection = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI.toString());
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("DB connection is working");
   } catch (error) {
     console.log(error);
